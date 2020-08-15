0bec5773e3f236de125b0b40bec940efbec81ded
indusam <victor@samper.mx>
2020-08-15 13:34:16 -0500

    sam_formulas: mostrar sólo ingredientes de la fórmula en el uso de ingrediente limitante

--- sam_formulas/models/models.py
+++ sam_formulas/models/models.py
@@ -9,17 +9,17 @@ class ListaMateriales(models.Model):
     x_porcentaje = fields.Float(string="%", digits=(3, 4))
     x_porcentaje_il = fields.Float(string="% IL", digits=(3, 4))
     x_cantidad_il = fields.Float(string="Cantidad Limitante", digits=(12, 4))
-
+    x_ingrediente_limitante = fields.Boolean(string="Il")
 
 class ListaMaterialesHeader(models.Model):
     _inherit = 'mrp.bom'
 
     product_qty = fields.Float(string="Cantidad", digits=(12, 4))
-    x_cantidad_il = fields.Float(string="Cantidad Limitante", digits=(12, 4))
-    x_ingrediente_limitante = fields.Many2one("mrp.bom.line",
-                                              string="Ingrediente limitante")
+    # x_cantidad_il = fields.Float(string="Cantidad Limitante", digits=(12, 4))
+    # x_ingrediente_limitante = fields.Many2one("mrp.bom.line",
+    #                                          string="Ingrediente limitante")
     x_formula = fields.Many2one("mrp.bom", string="Fórmula")
-    x_il = fields.Boolean(string="Il")
+
 
 class ReporteInventario(models.Model):
     _inherit = 'stock.quant'
