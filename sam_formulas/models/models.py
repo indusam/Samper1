3e7d5228a88ea1a0cdde885688a14aecb177add8
indusam <victor@samper.mx>
2020-08-25 12:28:38 -0500

    sam_formulas: x_ingrediente_limitante en mrp.bom

--- sam_formulas/models/models.py
+++ sam_formulas/models/models.py
@@ -23,7 +23,8 @@ class ListaMaterialesHeader(models.Model):
     @api.onchange('x_cantidad_il')
     def onchange_x_cantidad_il(self):
         for rec in self:
-            return {'domain': {'x_ingrediente_limitante': [('bom_id', '=', rec.product_tmpl_id.id)]}}
+            return {'domain': {'x_ingrediente_limitante':
+                                   [('parent_product_tmpl_id', '=', rec.product_tmpl_id.id)]}}
 
 
 class ReporteInventario(models.Model):
