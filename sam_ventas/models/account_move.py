b2c96eb12a27e7214ab61af3d421be36f2cb2483
indusam <victor@samper.mx>
2020-08-30 13:47:20 -0500

    sam_ventas: cálculo de comisión en account.move

--- sam_ventas/models/account_move.py
+++ sam_ventas/models/account_move.py
@@ -6,5 +6,13 @@ from odoo import models, fields, api
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    x_comision =fields.Float(string='Comisión', required=False)
+    @api.depends('amount_tax','amount_total','partner_id')
+    def calcula_comision(self):
+        nimporte = self.amount_total - self.amount_tax
+        n_pct_comision = env['res.partner'].search([('id', '=' , self.partner_id.id)], limit=1).x_pct_comision
+        self.x_comision = nimporte * (n_pct_comision / 100)
+
+    x_comision = fields.Float(string='Comisión',
+                             required=False,
+                             compute='calcula_comision')
 
