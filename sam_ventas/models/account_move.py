f9f6755ef1420a408c8f0830c1a7b4b87748fdcb
indusam <victor@samper.mx>
2020-08-30 14:03:50 -0500

    sam_ventas: cálculo de comisión en account.move

--- sam_ventas/models/account_move.py
+++ sam_ventas/models/account_move.py
@@ -9,7 +9,7 @@ class AccountMove(models.Model):
     @api.depends('amount_tax','amount_total','partner_id')
     def calcula_comision(self):
         nimporte = self.amount_total - self.amount_tax
-        n_pct_comision = env['res.partner'].search([('id', '=' , self.partner_id.id)], limit=1).x_pct_comision
+        n_pct_comision = self.env['res.partner'].search([('id', '=' , self.partner_id.id)], limit=1).x_pct_comision
         self.x_comision = nimporte * (n_pct_comision / 100)
 
     x_comision = fields.Float(string='Comisión',
