e2fc7a618cae8a11f2c0135b399f0c8d9421f818
indusam <victor@samper.mx>
2020-09-15 12:39:56 -0500

    sam_formulas: Calcula las cantidades de los ingredientes en base a ingrediente limitante en mrp.bom

--- sam_formulas/models/models.py
+++ sam_formulas/models/models.py
@@ -33,6 +33,7 @@ class ListaMaterialesHeader(models.Model):
     @api.onchange('x_cantidad_il')
     def onchange_x_cantidad_il(self):
         for rec in self:
+            raise Warning('self.product_tmpl_id: '+str(self.product_tmpl_id.id))
             return {'domain': {'x_ingrediente_limitante': [('bom_id', '=', self.product_tmpl_id.id)]}}
 
     @api.onchange('x_piezas')
