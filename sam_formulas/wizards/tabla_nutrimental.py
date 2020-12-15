c5a2c859e67a6772b45453bc9f53223f9d8f52a3
indusam <victor@samper.mx>
2020-12-15 12:39:26 -0600

    quité el wizard de tabla nutrimental

---
+++ sam_formulas/wizards/tabla_nutrimental.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# from odoo import models, fields, api
+
+# class Wizard(models.TransientModel):
+#     _name = 'Tabla Nutrimental'
+#     _description = 'Cálculo de tabla nutrimental'
+
+#     Producto = fields.Many2one(comodel_name="Producto", string="",
+#                                   required=True )
+
+#    cantidad = fields.Float(string="Cantidad",  required=False,
+#                            digits=(4, 4))
