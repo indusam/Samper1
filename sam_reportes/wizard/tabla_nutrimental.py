c1ab93441622a9081a40876563c4372bcd2619ff
indusam <administrador@ccuauhtemoc.com>
2021-05-18 09:38:24 -0500

    Update tabla_nutrimental.py

--- sam_reportes/wizard/tabla_nutrimental.py
+++ sam_reportes/wizard/tabla_nutrimental.py
@@ -37,7 +37,7 @@ class TablaNutrimental(models.TransientModel):
         
     @api.onchange('cantidad')
     def onchange_cantidad(self):
-        raise UserError(self.producto.product_id.x_pct_merma)
+        raise UserError('producto: '+self.producto+' '+str(self.producto.product_id)+str(self.producto.product_id.x_pct_merma))
         self.pct_merma = self.producto.x_pct_merma
 
 
