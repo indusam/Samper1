c188deb7d1ee89281c8cfddbacc119921e4dd656
indusam <administrador@ccuauhtemoc.com>
2021-05-18 13:42:59 -0500

    Update tabla_nutrimental.py

--- sam_reportes/wizard/tabla_nutrimental.py
+++ sam_reportes/wizard/tabla_nutrimental.py
@@ -33,7 +33,7 @@ class TablaNutrimental(models.TransientModel):
         nlista = self.producto.id
         # self.pct_merma = self.producto.product_tmpl_id.x_pct_merma
         if self.producto:
-            raise UserError('producto:' + str(self.producto.id.x_pct_merma))
+            raise UserError('producto:' + str(self.producto.product_tmpl_id.x_pct_merma))
              
         for rec in self:
             return {'domain': {'ing_limitante':
