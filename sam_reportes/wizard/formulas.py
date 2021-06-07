84c4a12dc7b3eb07d1c26bd043aba2d3419bdb12
indusam <administrador@ccuauhtemoc.com>
2021-06-07 15:01:30 -0500

    Update formulas.py

--- sam_reportes/wizard/formulas.py
+++ sam_reportes/wizard/formulas.py
@@ -78,6 +78,8 @@ class Formulas(models.TransientModel):
         # si se consolida la fórmula busca los ingredientes de los subproductos
         # y los suma a vals[]
         if self.consolidado:
+            raise UserError(ingredientes)
+            
             for ingrediente in ingredientes:
                 if ingrediente.product_tmpl_id.route_ids == 5:
                     raise UserError('El producto: '+ingrediente.product_tmpl_id.name+' se fabrica')
