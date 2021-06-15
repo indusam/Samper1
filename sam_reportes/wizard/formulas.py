0fd613fa398058a3675aad33e1d9022294c430a4
indusam <administrador@ccuauhtemoc.com>
2021-06-15 09:40:37 -0500

    Update formulas.py

--- sam_reportes/wizard/formulas.py
+++ sam_reportes/wizard/formulas.py
@@ -137,7 +137,7 @@ class Formulas(models.TransientModel):
             bom_consolidada = self.env['wizard.formulas'].search([])
             for ingrediente in bom_consolidada:
                 vals.append({
-                    'componente': ingrediente.ingr.id,
+                    'componente': ingrediente.ingr.name,
                     'cod_prov': ingrediente.cod_prov,
                     'cant_comp': ingrediente.cant_tot,
                     'unidad': ingrediente.ingr.uom_id.name,
