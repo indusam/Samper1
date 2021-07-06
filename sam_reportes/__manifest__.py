c79644a8137331c0a49fb3041c000230a4a293b5
indusam <administrador@ccuauhtemoc.com>
2021-07-06 11:25:17 -0500

    Update __manifest__.py

--- sam_reportes/__manifest__.py
+++ sam_reportes/__manifest__.py
@@ -36,7 +36,7 @@
         'views/contenido_energetico_reporte.xml',
         'wizard/tabla_nutrimental_view.xml',
         'wizard/formulas_view.xml',
-        'wizard/contenido_energetico_view',
+        'wizard/contenido_energetico_view.xml',
         'report/contenido_energetico_pdf.xml',
         'report/tabla_nutrimental_pdf.xml',
         'report/formulas_pdf.xml',
