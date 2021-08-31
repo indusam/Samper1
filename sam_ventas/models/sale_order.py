1fc2c64a23d18185c45dd3088988794c1c88817c
Becker-Vic96 <becker_tech@hotmail.com>
2021-08-31 11:05:20 -0500

    Update sale_order.py

--- sam_ventas/models/sale_order.py
+++ sam_ventas/models/sale_order.py
@@ -8,8 +8,8 @@ class SaleOrder(models.Model):
 
     @api.onchange('partner_id')
     def cliente_bloqueado(self):
-        if self.partner_id.x_studio_cliente_bloqueado.x_studio_motivo_de_bloqueo:
-            raise UserError('CLIENTE BLOQUEADO: ')
+        if self.partner_id.x_studio_cliente_bloqueado:
+            raise UserError('CLIENTE BLOQUEADO: ', self.partner_id.x_studio_motivo_de_bloqueo)
 
 
 
