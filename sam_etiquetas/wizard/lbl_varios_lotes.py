29c10ab1368d713ea3c88961c12edaea0617d9b1
Becker-Vic96 <becker_tech@hotmail.com>
2021-09-24 13:25:34 -0500

    Update lbl_varios_lotes.py

--- sam_etiquetas/wizard/lbl_varios_lotes.py
+++ sam_etiquetas/wizard/lbl_varios_lotes.py
@@ -22,8 +22,8 @@ class LblVariosLotes(models.TransientModel):
     lote3 = fields.Many2one('stock.production.lot', string="Lote 3")
     lote4 = fields.Many2one('stock.production.lot', string="Lote 4")
     lote5 = fields.Many2one('stock.production.lot', string="Lote 5")
-    especie = fields.Selection(string="Especie", selection=[('ave', 'Ave'), ('res', 'Res'),('cerdo', 'Cerdo'),
-                                                            ('cerdo_res', 'Cerdo y Res')], required=True, )
+    especie = fields.Selection(string="Especie", selection=[('ave', 'AVE'), ('res', 'RES'),('cerdo', 'CERDO'),
+                                                            ('cerdo_res', 'CERDO Y RES')], required=True, )
     cantidad = fields.Integer(string="Cantidad")
 
     # producto = fields.Many2one('mrp.bom', string="Producto")
