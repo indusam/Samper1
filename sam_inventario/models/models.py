00f07d07a525f0e3df80c6ae909ee64f883e2922
indusam <victor@samper.mx>
2021-12-14 16:16:20 -0600

    sam_inventario:

--- sam_inventario/models/models.py
+++ sam_inventario/models/models.py
@@ -3,14 +3,14 @@
 from odoo import models, fields, api
 
 # muestra la existencia del almacen de origen en los traslados
-class StockMove(models.Model):
-    _inherit = 'stock.move'
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
 
     x_exis_origen = fields.Float(string='Exis Origen')
 
-    @api.onchange('product_id')
-    def onchange_x_exis_origen(self):
-        if self.product_id:
-            nexis = self.env['stock.quant'].search([('product_id.id', '=', self.product_id.id), 
+    @api.onchange('move_line_ids_without_package')
+    def onchange_move_line_ids_without_package(self):
+        if self.move_line_ids_without_package:
+            nexis = self.env['stock.quant'].search([('product_id.id', '=', self.move_line_ids_without_package.id), 
                                                     ('location_id.id', '=', self.location_id.id)],limit=1).on_hand         
             self.x_exis_origen = nexis
