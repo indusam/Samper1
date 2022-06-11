c8feac774c970b927dbf2f511b55e40d9d54a17a
indusam <victor@samper.mx>
2022-06-11 13:29:17 -0500

    saldos clientes

--- sam_reportes/wizard/__init__.py
+++ sam_reportes/wizard/__init__.py
@@ -3,6 +3,6 @@
 # Tabla nutrimental, fórmulas, contenido energético, etc.
 # VBueno 1603202113:26
 
-from . import tabla_nutrimental, formulas, contenido_energetico, 
+from . import tabla_nutrimental, formulas, contenido_energetico
 from . import ant_saldos_detalle
 # saldos_clientes
