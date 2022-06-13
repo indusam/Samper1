5a4ad55c69c42e0b41e027b702887cc36f4da32b
indusam <victor@samper.mx>
2022-06-13 16:27:40 -0500

    saldos clientes

--- sam_reportes/wizard/saldos_clientes.py
+++ sam_reportes/wizard/saldos_clientes.py
@@ -23,7 +23,7 @@ class SaldosClientes(models.TransientModel):
     # Selecciona e imprime los saldos de los clientes.
     def imprime_saldos_clientes(self):
         
-        vals = []
+        saldos_de_clientes = []
         # Obtiene los clientes con saldo.
         clientes = self.env['res.partner'].search([('total_due', '>', 0)])
         
@@ -31,24 +31,26 @@ class SaldosClientes(models.TransientModel):
             raise UserError('No hay clientes con saldo.')
 
         # Recorre los clientes.
-        if clientes:
-            for cliente in clientes: 
-                # Guarda los valores en vals.
-                if cliente.total_due > 0:
-                    vals.append({
-                        'cliente': cliente.name,
-                        'nombre_comercial': cliente.x_nombre_comercial,
-                        'total_facturado': cliente.total_invoiced, 
-                        'total_adeudado': cliente.total_due,
-                        'total_vencido': cliente.total_overdue})
+        for cliente in clientes: 
+            # Guarda los valores en vals.
+            vals = {
+                    'cliente': cliente.name,
+                    'nombre_comercial': cliente.x_nombre_comercial,
+                    'total_facturado': cliente.total_invoiced, 
+                    'total_adeudado': cliente.total_due,
+                    'total_vencido': cliente.total_overdue
+                     }
+            saldos_de_clientes.append(vals)
 
-
-        data = {'ids': self.ids,
-                'model': self._name,
-                'vals': vals,
-                'fecha': datetime.date.today()
-                #'compania': self.env.company.name
-                }
+        #data = {'ids': self.ids,
+        #        'model': self._name,
+        #        'vals': vals,
+        #        'fecha': datetime.date.today(),
+        #         'compania': self.env.company.name
+        #        }
+        #return self.env['report'].get_action(self, 'saldos_clientes.saldos_clientes_report', data=data)
+        data = {'data': self.read()[0],
+                'saldos': saldos_de_clientes}
 
         return self.env.ref(
             'sam_reportes.saldos_clientes_reporte').report_action(self, data=data)
