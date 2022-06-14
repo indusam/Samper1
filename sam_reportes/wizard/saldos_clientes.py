9ae647db7e60fac7131678ae91d18315e87b88da
indusam <victor@samper.mx>
2022-06-14 11:46:42 -0500

    saldos clientes

--- sam_reportes/wizard/saldos_clientes.py
+++ sam_reportes/wizard/saldos_clientes.py
@@ -17,39 +17,43 @@ class SaldosClientes(models.TransientModel):
     _name = 'saldos.clientes.wizard'
     _description = 'Saldos de clientes'
 
-    #dfinal = datetime.date.today()
-    #fecha_corte = fields.Date(string="Fecha Corte", default=dfinal)
+    dfecha = datetime.date.today()
+    fecha = fields.Date(string="Fecha", default=dfecha)
     
     # Selecciona e imprime los saldos de los clientes.
     def imprime_saldos_clientes(self):
         
         saldos_de_clientes = []
         # Obtiene los clientes con saldo.
-        clientes = self.env['res.partner'].search([('total_due', '>', 0)])
+        clientes = self.env['res.partner'].search_read([('total_due', '>', 0)])
         
         if not clientes:
             raise UserError('No hay clientes con saldo.')
 
-        # Recorre los clientes.
-        for cliente in clientes: 
-            # Guarda los valores en vals.
-            vals = {
-                    'cliente': cliente.name,
-                    'nombre_comercial': cliente.x_nombre_comercial,
-                    'total_facturado': cliente.total_invoiced, 
-                    'total_adeudado': cliente.total_due,
-                    'total_vencido': cliente.total_overdue
-                     }
-            
-            saldos_de_clientes.append(vals)
+        data = {'form_data': self.read()[0],
+                'clientes': clientes}
 
-        data = {'ids': self.ids,
-                'model': self._name,
-                'vals': vals,
-                'fecha': datetime.date.today(),
-                'compania': self.env.company.name
-                }
-
-        # return self.env['report'].get_action(self, 'saldos_clientes.saldos_clientes_report', data=data)
         return self.env.ref('saldos_clientes.saldos_clientes_report').report_action(self,data=data)
-        
+
+        # # Recorre los clientes.
+        # for cliente in clientes: 
+        #     # Guarda los valores en vals.
+        #     vals = {
+        #             'cliente': cliente.name,
+        #             'nombre_comercial': cliente.x_nombre_comercial,
+        #             'total_facturado': cliente.total_invoiced, 
+        #             'total_adeudado': cliente.total_due,
+        #             'total_vencido': cliente.total_overdue
+        #              }
+            
+        #     saldos_de_clientes.append(vals)
+
+        # data = {'ids': self.ids,
+        #         'model': self._name,
+        #         'vals': vals,
+        #         'fecha': datetime.date.today(),
+        #         'compania': self.env.company.name
+        #         }
+
+        # # return self.env['report'].get_action(self, 'saldos_clientes.saldos_clientes_report', data=data)
+   
