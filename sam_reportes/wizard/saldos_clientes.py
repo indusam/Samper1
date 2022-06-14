faed9ab46442d0394359609fe3c24fd04476e46f
indusam <victor@samper.mx>
2022-06-13 19:37:23 -0500

    saldos clientes

--- sam_reportes/wizard/saldos_clientes.py
+++ sam_reportes/wizard/saldos_clientes.py
@@ -25,18 +25,12 @@ class SaldosClientes(models.TransientModel):
         
         saldos_de_clientes = []
         # Obtiene los clientes con saldo.
-        clientes = self.env['res.partner'].search_read([('total_due', '>', 0)])
+        clientes = self.env['res.partner'].search([('total_due', '>', 0)])
         
         if not clientes:
             raise UserError('No hay clientes con saldo.')
 
-        data = {'form_data': self.read()[0],
-                'saldos':clientes}
-
-        return self.env.ref(
-            'sam_reportes.saldos_clientes_reporte').report_action(self, data=data)
-
-        """ # Recorre los clientes.
+        # Recorre los clientes.
         for cliente in clientes: 
             # Guarda los valores en vals.
             vals = {
@@ -46,7 +40,7 @@ class SaldosClientes(models.TransientModel):
                     'total_adeudado': cliente.total_due,
                     'total_vencido': cliente.total_overdue
                      }
-            saldos_de_clientes.append(vals) """
+            saldos_de_clientes.append(vals)
 
         #data = {'ids': self.ids,
         #        'model': self._name,
@@ -55,7 +49,10 @@ class SaldosClientes(models.TransientModel):
         #         'compania': self.env.company.name
         #        }
         #return self.env['report'].get_action(self, 'saldos_clientes.saldos_clientes_report', data=data)
-        #saldos_data = {'saldos_data': self.read()[0],
-        #               'saldos': saldos_de_clientes}
-
+        
+        saldos_data = {'saldos_data': self.read()[0],
+                       'saldos': saldos_de_clientes}
+        
+        return self.env.ref(
+            'sam_reportes.saldos_clientes_reporte').report_action(self, data=saldos_data)
         
