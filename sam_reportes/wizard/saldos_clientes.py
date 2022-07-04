79d638002ef1080253073ba8e0ffc83e2c139fb2
indusam <victor@samper.mx>
2022-07-04 13:10:39 -0500

    saldos clientes

--- sam_reportes/wizard/saldos_clientes.py
+++ sam_reportes/wizard/saldos_clientes.py
@@ -24,7 +24,7 @@ class SaldosClientes(models.TransientModel):
     def imprime_saldos_clientes(self):
         
         # domain = [('total_due', '>', 0)]
-        saldos = self.env['res.partner'].search([('total_due', '!=', 0)])
+        saldos = self.env['res.partner'].search([('total_due', '>', 1)])
 
         raise UserError(saldos)    
 
