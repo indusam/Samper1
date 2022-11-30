34653c6ad522b4b7fc01bb20a95401ff79e902f6
indusam <victor@samper.mx>
2022-11-30 13:30:47 -0600

    sam_inventario: x_merma_pct en stoc.move

--- sam_inventario/models/stock_move.py
+++ sam_inventario/models/stock_move.py
@@ -10,26 +10,13 @@ class StockMove(models.Model):
     x_exis_origen = fields.Float(string='Exis Origen')
     x_merma_pct = fields.Float(string='% Merma',
                                digits=(3, 4))
-                               #compute='calcula_merma'))
 
     @api.onchange('product_id')
     def onchange_product_id(self):
         if self.product_id:
             nexis = self.env['stock.quant'].search([('product_id.id', '=', self.product_id.id), 
                                                     ('location_id.id', '=', self.location_id.id)],limit=1).quantity
-
             if nexis > 0:         
                 self.x_exis_origen = nexis
             
             self.name = 'Nuevo' # la descripcion es obligatoria
-
-'''
-    @api.depends('quantity_done', 'x_exis_origen')
-    def calcula_merma(self):
-        for reg in self:
-            if self.x_exis_origen > 0 and self.quantity_done > 0:
-                merma = 1 - (self.quantity_done / self.x_exis_origen)
-                raise UserError(merma)
-
-                reg.x_merma_pct= merma
-'''
