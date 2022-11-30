dd98184dc20005e096acef6ac2a1700d004e7d8e
indusam <victor@samper.mx>
2022-11-30 12:47:37 -0600

    sam_inventario: x_merma_pct en stoc.move

--- sam_inventario/models/stock_move.py
+++ sam_inventario/models/stock_move.py
@@ -28,4 +28,4 @@ class StockMove(models.Model):
     def calcula_merma(self):
         for reg in self:
             if self.x_exis_origen > 0 and self.quantity_done > 0:
-                reg.x_merma_pct = 1 - (self.quantity_done / self.x_exis_origen)
+                reg['x_merma_pct'] = 1 - (self.quantity_done / self.x_exis_origen)
