0375218fda4ccb536f0810cc5f75526e19b07ded
indusam <victor@samper.mx>
2023-01-24 09:57:49 -0600

    addenda liverpool v3.3

--- w_addenda_liverpool/__manifest__.py
+++ w_addenda_liverpool/__manifest__.py
@@ -44,7 +44,7 @@ This module adds the Liverpool addenda in signed invoices
         'l10n_mx_edi'
     ],
     'data': [
-        'data/addenda_liverpool.xml',
+        'data/addenda_liverpool_v40.xml',
         'views/inherit_res_partner_view.xml',
         'views/inherit_res_company_view.xml',
         'views/inherit_account_move_view.xml'
