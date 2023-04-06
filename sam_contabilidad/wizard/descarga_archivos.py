c03eca1b127b4318194ad26fa6bdfe88dc5910db
indusam <victor@samper.mx>
2023-04-06 11:15:10 -0600

    descarga_xml_pdf

--- sam_contabilidad/wizard/descarga_archivos.py
+++ sam_contabilidad/wizard/descarga_archivos.py
@@ -5,77 +5,27 @@
 # Descarga masiva de los xml y pdf de cfdi (facturas, notas de crédito, recibos de pago).
 # VBueno 0510202110:13
 
-
-import logging
-import zipfile
 import datetime
-from io import BytesIO
+import logging
+import requests
 
-from odoo import http
-from odoo.http import request
+from odoo import models, fields, http
+from odoo.exceptions import UserError
+import io
+from io import BytesIO
+import base64
+import os
+import zipfile
+
+
+
+# import mimetypes
+# from werkzeug.utils import redirect
 
 _logger = logging.getLogger(__name__)
 
-class DescargaXml(http.Controller):
-
-    _name = 'descarga_xml_wizard'
-    _description = 'Descarga XML'
-
-    @http.route('/download_invoices', type='http', auth='user')
-    def download_invoices(self, start_date=None, end_date=None, **kw):
-
-        end_date = datetime.date.today()
-        start_date = end_date - datetime.timedelta(days=31)
-
-        if not start_date or not end_date:
-            return "Debe especificar un rango de fechas válido."
-
-        invoices = request.env['account.move'].search([
-            ('type', '=', 'out_invoice'),
-            ('state', '=', 'posted'),
-            ('invoice_date', '>=', start_date),
-            ('invoice_date', '<=', end_date),
-        ])
-
-        if not invoices:
-            return "No se encontraron facturas para el rango de fechas especificado."
-
-        file_name = 'facturas_{}_{}.zip'.format(start_date, end_date)
-
-        # Generar archivo zip
-        zip_file = BytesIO()
-        with zipfile.ZipFile(zip_file, 'w') as my_zip:
-            for invoice in invoices:
-                file_data = invoice._get_report_from_name(
-                    'account.report_invoice_with_payments')
-                file_name = invoice.name.replace('/', '-') + '.pdf'
-                my_zip.writestr(file_name, file_data[0])
-        zip_file.seek(0)
-
-        # Descargar archivo zip
-        headers = [
-            ('Content-Type', 'application/octet-stream'),
-            ('Content-Disposition', 'attachment; filename=%s' % file_name),
-            ('Content-Length', len(zip_file.getvalue())),
-        ]
-
-        return request.make_response(zip_file.getvalue(), headers=headers)
-
-"""
-import datetime
-import requests                       
-import zipfile                        
-                                      
-from odoo import models, fields, http 
-from odoo.exceptions import UserError 
-import base64                         
-import io                             
-import os                             
-import mimetypes    
-# from werkzeug.utils import redirect                     
-
 class DescargaXml(models.TransientModel):
-    _name = 'descarga_xml_wizard'
+    _name = 'descarga_xml.wizard'
     _description = 'Descarga XML'
 
     dfinal = datetime.date.today()
@@ -83,67 +33,47 @@ class DescargaXml(models.TransientModel):
     facturas = fields.Boolean(string="Facturas", default=True )
     notas_credito = fields.Boolean(string="Notas de crédito", default=True )
     pagos = fields.Boolean(string="CFDI de pago", default=True )
-    marca = fields.Char(string="Marca", required=False, )
     fecha_inicial = fields.Date(string="Fecha inicial", required=True, default=dinicial)
     fecha_final = fields.Date(string="Fecha final", required=True, default=dfinal)
 
 
-    def dl(self,archivo):
-
-        action = {
-            'type': 'ir.actions.act_url',
-            'url': "web/content/?model=ir.attachment&id=" + str(
-                archivo.id) + "&filename_field=name&field=datas&download=true&name=" + archivo.name,
-            'target': 'self'
-        }
-
-        return action
-
-
-    # Descarga los xml
     def descargaxml(self):
-
         if not self.facturas and not self.pagos and not self.notas_credito:
             raise UserError('Debes seleccionar un tipo de comprobante')
 
+        if self.fecha_final < self.fecha_inicial:
+            raise UserError('Rango de fechas erroneo')
+
         if self.facturas:
-          cfdis = self.env['ir.attachment'].search([('res_model','=','account.move'),
-                                                       ('name','not ilike','RINV'),
-                                                       ('mimetype','=','text/plain'),
-                                                       ('create_date','>=',self.fecha_inicial),
-                                                       ('create_date','<=',self.fecha_final)])
+            #facturas
+            archivos = self.env['account.move'].search_read([('invoice_date','>=',self.fecha_inicial),
+                                                             ('invoice_date','<=',self.fecha_final),
+                                                             ('l10n_mx_edi_cfdi_uuid','=',True)])
 
-          if not cfdis:
-              raise UserError('No hay registros en ese rango de fechas')
+        if archivos:
 
-          for cfdi in cfdis:
-              descarga = self.dl(cfdi)
+            # Creamos una carpeta temporal para almacenar los archivos descargados
+            carpeta_temporal = os.path.expanduser('~/Temp/Facturas')
+            os.makedirs(carpeta_temporal, exist_ok=True)
 
-    # escribe una funcion para crear un archivo zip
-    # con los archivos de un directorio
+            for archivo in archivos:
 
-    def crear_zip(directorio, archivo):
-        import zipfile
-        import os
-        # crear el archivo zip
-        zip = zipfile.ZipFile(archivo,'w')
-        # recorrer el directorio
-        for raiz, dirs, files in os.walk(directorio):
-            # recorrer los archivos
-            for file in files:
-                # agregar el archivo al zip
-                zip.write(os.path.join(raiz, file))
-                # cerrar el archivo zip
-                zip.close()
+                nombre_archivo = archivo.name
 
-    # escribe una funcion para descargar el archivo zip
-    # de una URL
+                # Descargamos el archivo XML
+                archivo_xml = archivo.export_as_xml()
+                if archivo_xml:
+                    nombre_archivo_xml = f'{nombre_archivo}.xml'
+                    ruta_archivo_xml = os.path.join(carpeta_temporal,
+                                                    nombre_archivo_xml)
+                    with open(ruta_archivo_xml, 'wb') as f:
+                        f.write(base64.b64decode(archivo_xml))
 
-    def descargar_zip(url, archivo):
-        import urllib.request
-        # descargar el archivo
-        with urllib.request.urlopen(url) as response, open(archivo, 'wb') as out_file:
-            data = response.read()
-            out_file.write(data)
- 
-"""
+                # Descargamos el archivo PDF
+                archivo_pdf = archivo.get_report_data()[0]
+                if archivo_pdf:
+                    nombre_archivo_pdf = f'{nombre_archivo}.pdf'
+                    ruta_archivo_pdf = os.path.join(carpeta_temporal,
+                                                    nombre_archivo_pdf)
+                    with open(ruta_archivo_pdf, 'wb') as f:
+                        f.write(base64.b64decode(archivo_pdf))
