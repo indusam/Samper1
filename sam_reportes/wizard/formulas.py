e421c263ab48ce6796b5b3103f047eb1e902a27f
indusam <victor@samper.mx>
2023-09-20 12:10:50 -0600

    reporte de formulas

--- sam_reportes/wizard/formulas.py
+++ sam_reportes/wizard/formulas.py
@@ -264,9 +264,8 @@ class Formulas(models.TransientModel):
                 'cant_limitante':self.cant_limitante
                 }
 
-        self.cantidad = self.cant_limitante = self.partidas = self.ing_limitante = self.producto = 0
-        #self.env['wizard.formulas'].search([]).unlink()
-        #self.env['wizard.formulas'].create({})
+        self.env['wizard.formulas'].search([]).unlink()
+        self.env['wizard.formulas'].create({})
 
         return self.env.ref('sam_reportes.formulas_reporte').report_action(self, data=data)
 
