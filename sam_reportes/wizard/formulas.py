d72089362abf5273116ba0ba32ad38b99d34a16c
indusam <victor@samper.mx>
2023-10-19 16:46:38 -0600

    consolidacion recursiva de formulas

--- sam_reportes/wizard/formulas.py
+++ sam_reportes/wizard/formulas.py
@@ -115,7 +115,7 @@ class Formulas(models.TransientModel):
                     #        ncant = ncomponente_n1.cant_tot
                     #        ncomponente_n1.write({'cant_tot':(ncant_limitante * (componente_n1.x_porcentaje / 100)) + ncant})
 
-            if not ingrediente.product_id.bom_count:
+            if ingrediente.product_id.bom_count == 0:
 
                 raise UserError(ingrediente.product_id.name)
 
