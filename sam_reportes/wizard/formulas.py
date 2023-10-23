096bbdfc358978edced17f85e0973638c534edbc
indusam <victor@samper.mx>
2023-10-23 16:34:34 -0600

    consolidacion recursiva de formulas

--- sam_reportes/wizard/formulas.py
+++ sam_reportes/wizard/formulas.py
@@ -81,10 +81,8 @@ class Formulas(models.TransientModel):
                 bom_pf = self.env['mrp.bom'].search([(
                         'product_tmpl_id','=',ingrediente.product_tmpl_id.id)], limit=1).id
 
-                #bom_pf = self.env['mrp.bom.line'].search(
-                #        [('bom_id.id', '=',ingrediente.id)]).id
-
-                raise UserError(bom_pf)        
+                
+                raise UserError(ingrediente.product_id.name)        
 
                 subformula = self.env['mrp.bom.line'].search([
                         ('bom_id.id', '=', bom_pf)])
