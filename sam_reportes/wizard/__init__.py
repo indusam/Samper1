508f1d272b52f22f734335e4d374c6eed8f4e335
indusam <victor@samper.mx>
2024-02-23 12:30:06 -0600

    sin formulas_costo

--- sam_reportes/wizard/__init__.py
+++ sam_reportes/wizard/__init__.py
@@ -4,4 +4,4 @@
 # VBueno 1603202113:26
 
 from . import tabla_nutrimental, formulas, contenido_energetico, saldos_clientes
-from . import ant_saldos_detalle, formula_base_carnicos, formula_base_salmuera, formulas_costo
+from . import ant_saldos_detalle, formula_base_carnicos, formula_base_salmuera
