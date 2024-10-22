ee683cc9f9626a8f7bd06bdb693150c1ab6e84f1
indusam <victor@samper.mx>
2024-10-22 10:15:22 -0600

    attachment center

--- attachments_center/static/src/js/webKanbanRecord.js
+++ attachments_center/static/src/js/webKanbanRecord.js
@@ -1,75 +1,58 @@
 
 /** @odoo-module **/
 
-import { KanbanRecord } from "@web/views/kanban/kanban_record";
-import { session } from "@web/session";
-import { patch } from "@web/core/utils/patch";
+import { KanbanRecord } from 'web.KanbanRecord';
+import { session } from 'web.session';
 
-patch(KanbanRecord.prototype, 'attachments_center.webKanbanRecord', {
-    setup() {
-        this._super(...arguments);
-    },
-
-    // Añadimos eventos adicionales para los botones de previsualización
-    events: {
-        ...KanbanRecord.prototype.events,
+class CustomKanbanRecord extends KanbanRecord {
+    events = {
+        ...super.events,
         'click .on_preview_ms': '_onPreviewMSAttachment',
         'click .on_preview_google': '_onPreviewGoogleAttachment',
-    },
+    };
 
-    // Generar la URL del adjunto
     attachmentUrl() {
         return session.url('/web/content', {
-            id: this.record.data.id,
+            id: this.recordData.id,
             download: true,
         });
-    },
+    }
 
-    // Generar un token de acceso para los adjuntos privados
     async _generateAccessToken() {
         const access_token = await this.rpc({
             model: 'ir.attachment',
             method: 'generate_access_token',
-            args: [
-                [this.record.data.id]
-            ],
+            args: [[this.recordData.id]],
         });
         return access_token;
-    },
+    }
 
-    // Previsualizar adjuntos de Microsoft Office
     async _onPreviewMSAttachment(ev) {
         ev.stopPropagation();
         ev.preventDefault();
         let odoo_url = this.attachmentUrl();
-        if (this.record.data.type !== 'url' && !this.record.data.public) {
+        if (this.recordData.type !== 'url' && !this.recordData.public) {
             const access_token = await this._generateAccessToken();
             odoo_url += '&access_token=' + access_token;
         }
 
-        let url = 'https://view.officeapps.live.com/op/embed.aspx?src=' + encodeURIComponent(odoo_url);
-        if (this.record.data.type === 'url') {
-            url = this.record.data.url;
-        }
+        const url = 'https://view.officeapps.live.com/op/embed.aspx?src=' + encodeURIComponent(odoo_url);
+        window.open(this.recordData.type === 'url' ? this.recordData.url : url, '_blank');
+    }
 
-        window.open(url, '_blank');
-    },
-
-    // Previsualizar adjuntos de Google Docs
     async _onPreviewGoogleAttachment(ev) {
         ev.stopPropagation();
         ev.preventDefault();
         let odoo_url = this.attachmentUrl();
-        if (this.record.data.type !== 'url' && !this.record.data.public) {
+        if (this.recordData.type !== 'url' && !this.recordData.public) {
             const access_token = await this._generateAccessToken();
             odoo_url += '&access_token=' + access_token;
         }
 
-        let url = 'https://docs.google.com/viewer?url=' + encodeURIComponent(odoo_url);
-        if (this.record.data.type === 'url') {
-            url = this.record.data.url;
-        }
+        const url = 'https://docs.google.com/viewer?url=' + encodeURIComponent(odoo_url);
+        window.open(this.recordData.type === 'url' ? this.recordData.url : url, '_blank');
+    }
+}
 
-        window.open(url, '_blank');
-    },
-});
+export default CustomKanbanRecord;
+
