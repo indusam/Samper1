a8b1128f849a529e6dfa5b0a11b857379b785418
indusam <victor@samper.mx>
2024-12-17 11:53:42 -0600

    exis_origen en los traslados

--- sam_inventario/models/stock_move.py
+++ sam_inventario/models/stock_move.py
@@ -21,30 +21,7 @@ class StockMove(models.Model):
                 ('quantity','>',0)
             ], limit=1)
 
-            raise UserError(stock_quant.quantity)
-
             self.x_exis_origen = stock_quant.quantity if stock_quant else 0.0
 
             # La descripción es obligatoria, se establece un valor por defecto
             self.name = self.name or 'Nuevo'
-
-
-
-'''
-class StockMove(models.Model):
-    _inherit = 'stock.move'
-
-    x_exis_origen = fields.Float(string='Exis Origen')
-    x_merma_pct = fields.Float(string='% Merma',
-                               digits=(3, 2))
-
-    @api.onchange('product_id')
-    def onchange_product_id(self):
-        if self.product_id:
-            nexis = self.env['stock.quant'].search([('product_id.id', '=', self.product_id.id), 
-                                                    ('location_id.id', '=', self.location_id.id)],limit=1).quantity
-            if nexis > 0:         
-                self.x_exis_origen = nexis
-            
-            self.name = 'Nuevo' # la descripcion es obligatoria
-'''
