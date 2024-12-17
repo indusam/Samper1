09ae227594a95a0e3f62ea0c5fa2fa575bc49cbf
indusam <victor@samper.mx>
2024-12-17 11:44:49 -0600

    exis_origen en los traslados

--- sam_inventario/models/stock_move.py
+++ sam_inventario/models/stock_move.py
@@ -4,6 +4,30 @@ from odoo import models, fields, api
 from odoo.exceptions import UserError
 
 # muestra la existencia del almacen de origen en los traslados
+from odoo import models, fields, api
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    x_exis_origen = fields.Float(string='Exis Origen')
+    x_merma_pct = fields.Float(string='% Merma', digits=(3, 2))
+
+    @api.onchange('product_id')
+    def _onchange_product_id(self):
+        if self.product_id and self.location_id:
+            stock_quant = self.env['stock.quant'].search([
+                ('product_id', '=', self.product_id.id),
+                ('location_id', '=', self.location_id.id)
+            ], limit=1)
+
+            self.x_exis_origen = stock_quant.quantity if stock_quant else 0.0
+
+            # La descripción es obligatoria, se establece un valor por defecto
+            self.name = self.name or 'Nuevo'
+
+
+
+'''
 class StockMove(models.Model):
     _inherit = 'stock.move'
 
@@ -20,3 +44,4 @@ class StockMove(models.Model):
                 self.x_exis_origen = nexis
             
             self.name = 'Nuevo' # la descripcion es obligatoria
+'''
