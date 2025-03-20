e8178162765c709774f18a788b073eb4cf1b6111
indusam <victor@samper.mx>
2025-03-20 10:21:34 -0600

    fórmula_con_costo

--- sam_reportes/wizard/formulas_costo.py
+++ sam_reportes/wizard/formulas_costo.py
@@ -109,9 +109,9 @@ class FormulasCosto(models.TransientModel):
                 if subformula:
                     self.consolida_formula_costo(subformula, ncant_limitante, secuencia)
                 else:
-                    self.crear_ncomponente(ingrediente, secuencia, ncant_limitante)
+                    self.crear_ncomponente_costo(ingrediente, secuencia, ncant_limitante)
             else:
-                self.crear_ncomponente(ingrediente, secuencia, ncant_limitante)
+                self.crear_ncomponente_costo(ingrediente, secuencia, ncant_limitante)
 
         return
 
@@ -226,76 +226,3 @@ class FormulasCosto(models.TransientModel):
         return self.env.ref('sam_reportes.formulas_reporte').report_action(self, data=data)
         #report = self.env.ref('sam_reportes.formulas_reporte')
         #return report.report_action(self, data=data)
-
-'''
-    def consolida_formula(self, ingredientes, nqty, secuencia):
-
-        for ingrediente in ingredientes:
-
-            ncant_limitante = nqty * (ingrediente.x_porcentaje / 100)
-            # verifica que el ingrediente se fabrique.
-            if ingrediente.product_id.bom_count > 0: #tiene subformula
-
-                bom_pf = self.env['mrp.bom'].search([(
-                        'product_tmpl_id','=',ingrediente.product_tmpl_id.id)], limit=1).id
-
-                subformula = self.env['mrp.bom.line'].search([
-                        ('bom_id.id', '=', bom_pf)])
-
-                if subformula:               
-                    self.consolida_formula(subformula, ncant_limitante ,secuencia)
-
-                else:
-                    ncomponente = self.env['wizard.formulas'].search(
-                        [('ingr.id', '=', ingrediente.product_id.id),
-                         ('x_secuencia', '=', secuencia)])         
-
-                    if not ncomponente:
-                        codprov = self.get_codprov(ingrediente.product_id.product_tmpl_id.id)
-                        norden = self.get_orden(ingrediente.product_id.default_code)
-                        
-                        self.env['wizard.formulas'].create({
-                                        'x_secuencia':secuencia,
-                                        'ingr': ingrediente.product_id.id,
-                                        'cod_prov': codprov,
-                                        'cant_tot': ncant_limitante,
-                                        'unidad': ingrediente.product_id.uom_id.name,
-                                        'pct_formula': ingrediente.x_porcentaje,
-                                        'pct_categoria': ingrediente.x_porcentaje_categoria,
-                                        'x_orden': norden
-                            })
-
-                    if ncomponente:
-                        ncant = ncomponente.cant_tot
-                        nccomp = ncant_limitante
-                        ncant_tot = ncant + nccomp
-                        ncomponente.write({'cant_tot': ncant_tot})
-
-            else:
-
-                ncomponente = self.env['wizard.formulas'].search(
-                        [('ingr.id', '=', ingrediente.product_id.id),
-                         ('x_secuencia', '=', secuencia)])         
-
-                if not ncomponente:
-                    codprov = self.get_codprov(ingrediente.product_id.product_tmpl_id.id)   
-                    norden = self.get_orden(ingrediente.product_id.default_code)
-                       
-                    self.env['wizard.formulas'].create({
-                                    'x_secuencia':secuencia,
-                                    'ingr': ingrediente.product_id.id,
-                                    'cod_prov': codprov,
-                                    'cant_tot': ncant_limitante,
-                                    'unidad': ingrediente.product_id.uom_id.name,
-                                    'pct_formula': ingrediente.x_porcentaje,
-                                    'pct_categoria': ingrediente.x_porcentaje_categoria,
-                                    'x_orden': norden
-                        })
-
-                if ncomponente:
-                    ncant = ncomponente.cant_tot
-                    nccomp = ncant_limitante
-                    ncant_tot = ncant + nccomp
-                    ncomponente.write({'cant_tot': ncant_tot})
-        return
-'''
