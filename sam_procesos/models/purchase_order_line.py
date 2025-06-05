23f5219a8d5cf2a104fa665f6af795a6bb909420
Grupo El Rey <administrador@ccuauhtemoc.com>
2025-06-05 09:47:06 -0600

    no message

--- sam_procesos/models/purchase_order_line.py
+++ sam_procesos/models/purchase_order_line.py
@@ -1,34 +1,27 @@
 from odoo import models, fields, api, _
-from odoo.exceptions import ValidationError
+from odoo.exceptions import ValidationError, UserError
 
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
-    def validate_product_cost(self):
+    @api.onchange('product_id', 'price_unit')
+    def _onchange_product_id_validate_cost(self):
         """
         Valida que el costo del producto coincida con el registrado en product.supplierinfo
+        cuando se cambia el producto o el precio unitario
         """
-        for line in self:
-            if not line.product_id or not line.order_id.partner_id:
-                continue
-                
-            supplier_info = self.env['product.supplierinfo'].search([
-                ('product_tmpl_id', '=', line.product_id.product_tmpl_id.id),
-                ('partner_id', '=', line.order_id.partner_id.id)
-            ], limit=1)
+        if not self.product_id or not self.order_id.partner_id:
+            return
             
-            if supplier_info and supplier_info.price != line.price_unit:
-                raise ValidationError(_("Costo no autorizado. El costo debe ser %s según la información del proveedor." % supplier_info.price))
-        return True
-    
-    @api.model
-    def create(self, vals):
-        res = super(PurchaseOrderLine, self).create(vals)
-        res.validate_product_cost()
-        return res
-    
-    def write(self, vals):
-        res = super(PurchaseOrderLine, self).write(vals)
-        if 'price_unit' in vals:
-            self.validate_product_cost()
-        return res
+        supplier_info = self.env['product.supplierinfo'].search([
+            ('product_tmpl_id', '=', self.product_id.product_tmpl_id.id),
+            ('partner_id', '=', self.order_id.partner_id.id)
+        ], limit=1)
+        
+        if supplier_info and self.price_unit and self.price_unit != supplier_info.price:
+            warning = {
+                'title': _('Costo no autorizado'),
+                'message': _('El costo del producto %s debe ser %s según la información del proveedor.') % (self.product_id.name, supplier_info.price),
+            }
+            self.price_unit = supplier_info.price
+            return {'warning': warning}
