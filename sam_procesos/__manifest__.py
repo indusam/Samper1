0e09c0c384770ea2c18b5c8d2b594183f6524d87
Grupo El Rey <administrador@ccuauhtemoc.com>
2025-06-05 09:31:46 -0600

    validar costos de la compra vs costos autorizados al proveedor

---
+++ sam_procesos/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Samper - Procesos',
+    'version': '16.0.1.0.0',
+    'category': 'Purchase',
+    'summary': 'Módulo para procesos personalizados de Samper',
+    'description': """
+        Módulo que incluye validaciones personalizadas para procesos de compras
+    """,
+    'author': 'Samper',
+    'website': 'https://www.samper.com',
+    'depends': ['purchase'],
+    'data': [
+        # Aquí se agregarán los archivos XML de vistas si son necesarios
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
