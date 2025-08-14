c8efe0acd435690419540328e208f46edda8c7f6
Grupo El Rey <administrador@ccuauhtemoc.com>
2025-08-14 11:31:11 -0600

    no message

--- sam_quantity_validator/__init__.py
+++ sam_quantity_validator/__init__.py
@@ -1,15 +1,2 @@
 from . import models
-
-def pre_init_hook(cr):
-    """Pre-init hook for module installation."""
-    pass
-
-def post_init_hook(cr, registry):
-    """Post-init hook for module installation.
-    
-    Args:
-        cr: database cursor
-        registry: Odoo model registry
-    """
-    pass
 # The actual imports will be done when the registry is built
