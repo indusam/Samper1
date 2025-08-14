c227f6374102eef7f561eac7d42a5d61f708b508
Grupo El Rey <administrador@ccuauhtemoc.com>
2025-08-14 10:07:12 -0600

    no message

--- sam_quantity_validator/__init__.py
+++ sam_quantity_validator/__init__.py
@@ -1,5 +1,10 @@
-from . import stock_move
-from . import stock_quant
-from . import mrp_production
-from . import stock_lot
-from . import stock_picking
+def pre_init_hook(cr):
+    """Pre-init hook to avoid circular imports."""
+    pass
+
+def post_init_hook(cr, registry):
+    """Post-init hook to import models after registry is ready."""
+    from . import models
+
+# This is the standard Odoo way to import models
+# The actual imports will be done when the registry is built
---
+++ sam_quantity_validator/models/__init__.py
@@ -0,0 +1,6 @@
+# Import all models here to make them available when the module is loaded
+from . import stock_move
+from . import stock_quant
+from . import mrp_production
+from . import stock_lot
+from . import stock_picking
