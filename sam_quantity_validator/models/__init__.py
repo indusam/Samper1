00c95d4b2df0aeb80df1bb3ea4e1529e0025192a
Grupo El Rey <administrador@ccuauhtemoc.com>
2025-09-18 10:33:14 -0600

    se desinstaló el modulo "sam_quantity_validator"

--- sam_quantity_validator/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# Import all models here to make them available when the module is loaded
-from . import stock_move
-from . import stock_quant
-from . import mrp_production
-from . import stock_lot
-from . import stock_picking
-from . import server_actions
-from . import mrp_consumption_warning_line
--- sam_quantity_validator/models/mrp_consumption_warning_line.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class MrpConsumptionWarningLine(models.TransientModel):
-    """Extiende mrp.consumption.warning.line para modificar la precisión de los campos flotantes."""
-    _inherit = 'mrp.consumption.warning.line'
-    _name = 'mrp.consumption.warning.line'  
-
-    product_consumed_qty_uom = fields.Float(digits=(16, 6))
-    product_expected_qty_uom = fields.Float(digits=(16, 6))
