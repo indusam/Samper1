d136e3660267df492ca4b4c1c19ebe0884a5bc5e
Grupo El Rey <administrador@ccuauhtemoc.com>
2026-04-16 11:25:52 -0600

    no message

--- stock_no_negative/models/product.py
+++ stock_no_negative/models/product.py
@@ -1,6 +1,7 @@
 # ?? 2015-2016 Akretion (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# 
 
 from odoo import models, fields
 
